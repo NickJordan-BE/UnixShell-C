d8731900be4ff45fc8030b6c15c5c4ea2e1afbf3
NickJordan-BE <nikjordan525@gmail.com>
2025-07-03 15:50:06 -0700

    Dev: Implement error and formatting for echo command

--- src/commands.c
+++ src/commands.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
-
+#include <string.h>
 
 void echo_command(int argc, char **argv) {
+    if (argv[0][0] != '"' || argv[argc - 1][strlen(argv[argc - 1]) - 1] !=  '"') {
+        fprintf(stderr, "Unknown\n");
+        return;
+    }
+
+    memmove(argv[0], argv[0] + 1, strlen(argv[0]));
+
+    if (argc == 1) {
+        argv[0][strlen(argv[0]) - 1] = '\0';
+    } else {
+        argv[argc - 1][strlen(argv[argc - 1]) - 1] = '\0';
+    }
+
     for (int i = 0; i < argc; i++) {
         printf("%s ", argv[i]);
     }
